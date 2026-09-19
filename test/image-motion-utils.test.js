c89697ab3461a3b2b27677a3d0148e80622976e8
truongnguyen217011-ai <truongnguyen217011@gmail.com>
2026-09-19 16:58:41 +0700

    Limit image keyframes to centered zoom

--- test/image-motion-utils.test.js
+++ test/image-motion-utils.test.js
@@ -2,11 +2,13 @@ import test from "node:test";
 import assert from "node:assert/strict";
 import { buildImageMotionFilter } from "../image-motion-utils.js";
 
-test("keyframe luân phiên sáu chuyển động khác nhau", () => {
-  const filters = Array.from({ length:6 }, (_, index) => buildImageMotionFilter({ index, strength:8, duration:5, width:1920, height:1080 }));
-  assert.equal(new Set(filters).size, 6);
+test("keyframe chỉ luân phiên zoom vào và zoom ra từ tâm", () => {
+  const filters = Array.from({ length:4 }, (_, index) => buildImageMotionFilter({ index, strength:8, duration:5, width:1920, height:1080 }));
+  assert.equal(new Set(filters).size, 2);
   assert.match(filters[0], /1\+0\.0800/);
   assert.match(filters[1], /1\.0800-0\.0800/);
-  assert.match(filters[2], /\*min\(on\/150,1\)/);
-  assert.match(filters[3], /1-min\(on\/150,1\)/);
+  for (const filter of filters) {
+    assert.match(filter, /x='\(iw-iw\/zoom\)\/2'/);
+    assert.match(filter, /y='\(ih-ih\/zoom\)\/2'/);
+  }
 });
